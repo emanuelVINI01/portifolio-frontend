d48eac4aa99325c411bd4bd124dbafb570131c4a
Emanuel <emanuelmissena12121@gmail.com>
2023-08-20 07:50:02 -0300

    Changed footer and index.html

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,8 +6,10 @@ export default function Footer() {
         <Container bg={"blackAlpha.300"} minW={"100%"} minH={"10vh"} bottom={0} p={5} mx={"auto"}>
             <AbsoluteCenter axis={"horizontal"}>
                 <Text textAlign={"center"} fontSize={"2xl"} fontFamily={"monospace"} fontWeight={"700"}>
-                    Copyright 2023 emanuelVINI (emanuelvini),
-                    Made with much love in 19/08/2023 with 5 hours.
+                    © Copyright 2023 emanuelVINI.
+                </Text>
+                <Text textAlign={"center"} fontSize={"2xl"} fontFamily={"monospace"} fontWeight={"700"}>
+                    Made with much ❤️ in 19/08/2023 with 5 hours.
                 </Text>
             </AbsoluteCenter>
         </Container>
