544b7c1eacda0221e8b6f0767d240621f55fcd65
Emanuel <80303138+emanuelVINI01@users.noreply.github.com>
2024-01-27 16:46:33 -0300

    Now images will work fine

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Container, SimpleGrid, ChakraProvider, extendTheme, Divider, Text, Stack, SlideFade, Fade } from '@chakra-ui/react';
+import { Container, SimpleGrid, ChakraProvider, extendTheme, Divider, Text, Stack, SlideFade, Fade, GridItem } from '@chakra-ui/react';
 import React, { useState } from 'react';
 import Navbar from './components/Navbar';
 import QuestionCard from './components/QuestionCard';
@@ -27,108 +27,99 @@ function App() {
 
   const [applicationType, setApplicationType] = useState("plugin")
 
-  const language = {
-    "questions": {
-      "why_you_started": "Why you start?",
-      "because_i_started": "When I was 10 years old, I started playing a game called Minecraft, this game has a server that you can create plugins in Java, and that stimulated me to learn to program to create my own server.",
-      "which_you_create": "Which you can code?",
-      "skills": "I have my bests skills in Java, C++, Python and JavaScript, expert in bukkit. About frameworks, I use much React and NextJS because are perfect to make a speed app, you can check my projects down.",
-
-    }
-  }
 
   const applications = [
     {
       name: "FeastLobby",
       image: "/images/feastplugins.png",
       url: "https://github.com/emanuelVINI01/FeastLobby",
-      description: "A very lightning lobby plugin for 1.8-1.20.1 with many resources, like NPCs.",
+      description: "I've created an exceptionally fast lobby plugin compatible with versions 1.8 to 1.20.1. This plugin comes packed with a plethora of resources, including NPCs, to enhance the user experience and engagement within the lobby environment.",
       type: "plugin"
     },
     {
       name: "FeastCore",
       image: "/images/feastplugins.png",
       url: "https://github.com/emanuelVINI01/FeastCore",
-      description: "A core for FeastPlugins with many APIs, like Dependency finder.",
+      description: "I've developed a robust core for FeastPlugins, equipped with a variety of APIs, including a Dependency Finder. This core provides a solid foundation for building and expanding functionalities within the FeastPlugins ecosystem.",
       type: "plugin"
     },
     {
-      name: "FeastBoard",
+      name: "FeastScore",
       image: "/images/feastplugins.png",
-      url: "https://github.com/emanuelVINI01/FeastBoard",
-      description: "A simple scoreboard plugin with hooks, like SimpleClans and PlotSquared",
+      url: "https://github.com/emanuelVINI01/FeastScore",
+      description: "I have crafted a user-friendly scoreboard plugin enriched with hooks, similar to the functionality seen in plugins such as SimpleClans and PlotSquared.",
       type: "plugin"
     },
     {
       name: "DVDuels",
-      image: "https://cdn.discordapp.com/icons/617294498535309322/a_33392ff612476ca3edab038e99b39241.webp",
+      image: "/images/devroom.webp",
       url: "https://github.com/emanuelVINI01/DVDuels",
-      description: "A simple duels plugin with configurable kits for DevRoom trial phase, made with 7 days timeframe.",
+      description: "During the DevRoom trial phase, I successfully designed a straightforward duels plugin that offers configurable kits. This intricate project was completed within a span of 7 days.",
       type: "plugin"
     },
     {
       name: "EMTrocar",
-      image: "https://cdn.discordapp.com/icons/1033504407263907941/a_d51c77d3b910998b81d439010be60502.webp",
+      image: "/images/zc.webp",
       url: "https://github.com/emanuelVINI01/EMTrocar",
-      description: "A simple plugin to make trades for ZappyCraft verification, made with 2 hours timeframe.",
+      description: "A swiftly developed a basic plugin for ZappyCraft verification purposes, focused on facilitating trades. This project was accomplished within a tight timeframe of just 2 hours.",
       type: "plugin"
     },
     {
       name: "MultiServer",
       image: "/images/minecraft.png",
       url: "https://github.com/emanuelVINI01/MultiServer-API",
-      description: "A API where you can make plugins with one code and run in many platforms, read wiki for more.",
+      description: "An API has been developed, allowing you to create plugins with a single codebase that can be executed across multiple platforms. For more detailed information, please refer to the provided wiki.",
       type: "plugin"
     },
     {
       name: "RyzenShopBot",
       image: "/images/discord.png",
       url: "https://github.com/emanuelVINI01/RyzenShopBot",
-      description: "A Discord Bot with many systems, like economy, xp and others using typescript.",
+      description: "I've crafted a versatile Discord Bot employing TypeScript, featuring a multitude of systems such as economy, XP tracking, and various other functionalities.",
       type: "bot"
     },
     {
       name: "ComuniMineBot",
       image: "/images/discord.png",
       url: "https://github.com/emanuelVINI01/ComuniMineBot",
-      description: "A Discord Bot with ticket system, economy and moderation commands and anothers system.",
+      description: "I designed a comprehensive Discord Bot equipped with a ticket system, economy features, moderation commands, and various other systems to enhance its functionality.",
       type: "bot"
     },
     {
       name: "MineFeast",
       image: "/images/discord.png",
       url: "https://github.com/emanuelVINI01/MinecraftFeastBot",
-      description: "A Discord Bot with ticket system, minecraft server query and skin query, made for a community.",
+      description: "I developed a Discord Bot tailored for a community, which includes a ticket system, Minecraft server query, and skin query functionalities.",
       type: "bot"
     },
     {
       name: "Ryzen",
       image: "/images/ryzen.png",
       url: "https://github.com/emanuelVINI01/RyzenSite",
-      description: "A WebSite for my old hosting was closed in Jun 2022, the site was made with TypeScript and NextJS.",
+      description: "A website for my former hosting was taken down in June 2022. The site had been developed using TypeScript and NextJS.",
       type: "website"
     },
     {
       name: "Portfolio",
       image: "/images/me.webp",
       url: "https://github.com/emanuelVINI01/portfolio-frontend",
-      description: "The site where you stay now, made in 5 hours timeframe with React and Typescript, and Chakra UI.",
+      description: "The current website you are viewing was created within a 5-hour timeframe using React, TypeScript, and Chakra UI.",
       type: "website"
     },
   ]
   return (
-    <ChakraProvider theme={theme}>
+    <ChakraProvider theme={theme} >
       <Navbar />
       <Divider />
-      <Container p={5} minH={"30vh"} display={"flex"} justifyContent={"center"} alignItems={"center"}>
-        <SimpleGrid mt={10} minW={"100vh"} columns={[1, 1, 3]} spacingX={5} spacingY={5}>
-          <QuestionCard title={"Why you start?"} answer={"When I was 10 years old, I started playing a game called Minecraft, this game has a server that you can create plugins in Java, and that stimulated me to learn to program to create my own server."} />
-          <QuestionCard title={"Which you can code?"} answer={"I have my bests skills in Java, C++, Python and JavaScript, expert in bukkit. About frameworks, I use much React and NextJS because are perfect to make a speed app, you can check my projects down."} />
-          <QuestionCard title={"How I can order?"} answer={"You can tell me in discord, or using open stores like DevRoom and Arcane. The timeframe and price is variable, depend of your request and the complexity of this, contact me to get a quote about your project!"} />
+      <Container minH={"30vh"} display={"flex"} justifyContent={"center"} alignItems={"center"}>
+        <SimpleGrid mt={10} minW={"100vh"} columns={[1, 1, 3]} spacingX={5} spacingY={5} p={5}>
+          <QuestionCard title={"Why you was start?"} answer={"When I was 10 years old, I began playing a game called Minecraft. This game has a server in which you can create plugins using Java. This stimulated me to learn programming in order to create my own server."} />
+          <QuestionCard title={"Which you can code?"} answer={"I am most proficient in Java, C++, Python, and JavaScript, and I am an expert in Bukkit. When it comes to frameworks, I heavily utilize React and NextJS because they are perfect for creating fast applications. You can check out my projects below."} />
+          <QuestionCard title={"How I can order?"} answer={"You can reach out to me on Discord or utilize open platforms such as DevRoom and Arcane. The timeframe and pricing are variable, depending on your specific requirements and the complexity involved. Feel free to contact me to receive a personalized quote for your project!"} />
         </SimpleGrid>
       </Container>
       <Divider />
-      <Container p={5} mt={5} minH={"30vh"} display={"flex"} justifyContent={"center"} alignItems={"center"}>
+      <Container p={10} mt={5} minH={"30vh"} display={"flex"} justifyContent={"center"} alignItems={"center"}>
         <Stack>
           <Text fontFamily={"monospace"} fontSize={"3xl"} fontWeight={600} textAlign={"center"}>My Projects</Text>
           <SimpleGrid mt={10} minW={"100vh"} columns={[1, 1, 3]} spacingX={5} spacingY={5}>
@@ -143,19 +134,15 @@ function App() {
             }} />
           </SimpleGrid>
           <Divider />
-          <SimpleGrid mt={5} minW={"100vh"} columns={[1, 1, 3]} spacingX={5} spacingY={5}>
-            {applications.filter(p => p.type == applicationType).map(app => {
-              return (
-                <Fade in={true}>
-                  <Application {...app} />
-                </Fade>
-              )
-            })}
+          <SimpleGrid mt={10} minW={"100vh"} columns={[1, 1, 3]} spacingX={5} spacingY={5}>
+            {applications.filter(p => p.type === applicationType).map(app => (
+              <Application image={app.image} url={app.url} key={app.name} name={app.name} description={app.description} />
+            ))}
           </SimpleGrid>
         </Stack>
 
       </Container>
-      <Divider mt={10}/>
+      <Divider mt={10} />
       <Footer />
     </ChakraProvider>
   );
